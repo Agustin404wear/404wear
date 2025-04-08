7aed5a6a8d90259d1b3ad8eef68d90259e720fa2
Agustin404wear <agusparaversifunciona@gmail.com>
2025-04-08 10:45:48 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.post('/api/pedido', (req, res) => {
+  console.log("Pedido recibido:", req.body);
+  res.json({ mensaje: "¡Pedido recibido correctamente!" });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en http://localhost:" + PORT);
+});
