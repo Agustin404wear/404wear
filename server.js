abbf262363e7f21b8200e7a15bcf567067e3bfd8
Agustin404wear <agusparaversifunciona@gmail.com>
2025-04-09 08:09:55 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const helmet = require('helmet');
+const cors = require('cors');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
+app.use(cors({ origin: process.env.DOMAIN || '*' }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/api/pedido', (req, res) => {
-  console.log("Pedido recibido:", req.body);
-  res.json({ mensaje: "¡Pedido recibido correctamente!" });
+  console.log('Pedido recibido:', req.body);
+  res.json({ mensaje: '¡Pedido recibido correctamente!' });
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Servidor corriendo en http://localhost:" + PORT);
-});
+app.listen(PORT, () => console.log('Servidor en http://localhost:' + PORT));
